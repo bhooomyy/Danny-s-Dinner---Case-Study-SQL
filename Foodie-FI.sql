a84d2fae96882ad4785bfdb3ad60f686178468ae
bhooomyy <bhattbhoomi0111@gmail.com>
2026-02-17 16:05:53 -0500

    feat: analyze monthly distribution of trial plan starts
    - Filtered trial plan (plan_id = 0).
    - Grouped by month extracted from start_date.
    - Computed count of trial subscriptions per month.
    - Provides monthly onboarding trend.

--- Foodie-FI.sql
+++ Foodie-FI.sql
@@ -1,3 +1,11 @@
 --1. How many customers has Foodie-Fi ever had?
 select count(distinct customer_id) from subscriptions;
 
+--2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+select 
+extract(month from start_date) as monthlywise_subscription_trial,
+count(customer_id) as cnt_of_people_purchased_trial 
+from subscriptions 
+where plan_id=0 
+group by extract(month from start_date);
+
